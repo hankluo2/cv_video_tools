22e0d4d1133f589ef141e169a0233afce1e5bd0e
hankluo2 <lh313hk@163.com>
2021-07-05 13:55:36 +0800

    align imgsz to write a whole video

--- makevideo.py
+++ makevideo.py
@@ -11,13 +11,15 @@ def makeVideo(input, output, fps):
 
     size = cv2.imread(filelist[0]).shape[:2][::-1]
     video = cv2.VideoWriter(output, cv2.VideoWriter_fourcc('M', 'P', '4', 'V'), fps, size, True)
-    # video = cv2.VideoWriter(path + '/video.avi', cv2.VideoWriter_fourcc('X', 'V', 'I', 'D'), fps, size, True)
+    # video = cv2.VideoWriter(output, cv2.VideoWriter_fourcc('X', 'V', 'I', 'D'), fps, size, True)
 
     for item in filelist:
-        print(item)
         if item.endswith('.jpg'):
-            print(item)
             img = cv2.imread(item)
+            if size != img.shape[:2][::-1]:
+                img = cv2.resize(img, (size))
+            # cv2.imshow('video', img)
+            # cv2.waitKey(50)
             video.write(img)
 
     video.release()
@@ -27,9 +29,9 @@ def makeVideo(input, output, fps):
 
 def parse():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input', type=str, default='', help='Input frames path')
-    parser.add_argument('--output', type=str, default='', help='Video output path')
-    parser.add_argument('--fps', type=int, default=3, help='frame per second')
+    parser.add_argument('--input', type=str, default='/home/roxy/lab/Traffics/TrafficMonitor/runs/detect/exp20/det', help='Input frames path')
+    parser.add_argument('--output', type=str, default='./015.mp4', help='Video output path')
+    parser.add_argument('--fps', type=int, default=20, help='frame per second')
     opt = parser.parse_args()
     return opt
 
@@ -37,4 +39,4 @@ def parse():
 if __name__ == '__main__':
     # path = './optflow'
     opt = parse()
-    makeVideo(opt.input, opt.output, opt.fps)
+    makeVideo(**vars(opt))
